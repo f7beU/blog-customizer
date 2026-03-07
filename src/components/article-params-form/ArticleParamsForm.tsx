f642851a95cbf007dcbb2e995d51314929c992f3
Vladimir <f7beu@yandex.ru>
2026-03-07 22:24:36 +0300

    Исправил грубые ошибки

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,3 +1,4 @@
+import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import { RadioGroup } from 'src/ui/radio-group';
@@ -15,25 +16,47 @@ import {
 	ArticleStateType,
 } from 'src/constants/articleProps';
 
-import { forwardRef, useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
 
 type ArticleParamsFormProps = {
-	// открыт ли сайдбар
-	isOpen: boolean;
 	// текущие применённые настройки
 	currentSettings: ArticleStateType;
 	// функция для применения настроек
 	applySettings: (settings: ArticleStateType) => void;
 };
 
-export const ArticleParamsForm = forwardRef<
-	HTMLElement,
-	ArticleParamsFormProps
->(({ isOpen, currentSettings, applySettings }, ref) => {
+export const ArticleParamsForm = ({
+	currentSettings,
+	applySettings,
+}: ArticleParamsFormProps) => {
 	console.log('Форма открылась');
+
+	// состояние для открытия сайдбара
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	// console.log('Состояние isSidebarOpen = ', isSidebarOpen);
+
+	// создаём ref формы
+	const formRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		// если панель закрыта, ничего не делаем
+		if (!isSidebarOpen) return;
+
+		const handleClickOutside = (event: MouseEvent) => {
+			// проверяем, был ли клик внутри нашей формы
+			if (formRef.current && !formRef.current.contains(event.target as Node)) {
+				// если клик снаружи — закрываем
+				setIsSidebarOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => document.removeEventListener('mousedown', handleClickOutside);
+		// эффект перезапускается при изменении isOpen
+	}, [isSidebarOpen]);
 
 	// состояние формы при открытии с выбранными настройками
 	const [formSettings, setFormSettings] =
@@ -59,78 +82,82 @@ export const ArticleParamsForm = forwardRef<
 
 	return (
 		<>
-			<aside
-				ref={ref}
-				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
-				<form
-					className={styles.form}
-					onSubmit={(event) => event.preventDefault()}>
-					<Text size={31} weight={800}>
-						Задайте параметры
-					</Text>
-					<Select
-						title='Шрифт'
-						options={fontFamilyOptions}
-						selected={formSettings.fontFamilyOption}
-						onChange={(value) =>
-							setFormSettings({ ...formSettings, fontFamilyOption: value })
-						}
-						placeholder='Выберите шрифт'
-					/>
-					<RadioGroup
-						name='Размер шрифта'
-						title='Размер шрифта'
-						options={fontSizeOptions}
-						selected={formSettings.fontSizeOption}
-						onChange={(value) =>
-							setFormSettings({ ...formSettings, fontSizeOption: value })
-						}
-					/>
-					<Select
-						title='Цвет шрифта'
-						options={fontColors}
-						selected={formSettings.fontColor}
-						onChange={(value) =>
-							setFormSettings({ ...formSettings, fontColor: value })
-						}
-						placeholder='Выберите цвет шрифта'
-					/>
-					<Separator />
-					<Select
-						title='Цвет фона'
-						options={backgroundColors}
-						selected={formSettings.backgroundColor}
-						onChange={(value) =>
-							setFormSettings({ ...formSettings, backgroundColor: value })
-						}
-						placeholder='Выберите цвет фона'
-					/>
-					<Select
-						title='Ширина контента'
-						options={contentWidthArr}
-						selected={formSettings.contentWidth}
-						onChange={(value) =>
-							setFormSettings({ ...formSettings, contentWidth: value })
-						}
-						placeholder='Выберите ширину контента'
-					/>
-					<div className={styles.bottomContainer}>
-						<Button
-							title='Сбросить'
-							htmlType='reset'
-							type='clear'
-							onClick={handleReset}
+			<div ref={formRef}>
+				<ArrowButton
+					isOpen={isSidebarOpen}
+					onClick={() => {
+						// console.log("клик из формы, isSidebarOpen: ", isSidebarOpen);
+						setIsSidebarOpen((prev) => !prev);
+					}}
+				/>
+				<aside
+					className={clsx(styles.container, {
+						[styles.container_open]: isSidebarOpen,
+					})}>
+					<form
+						className={styles.form}
+						onSubmit={(event) => {
+							event.preventDefault();
+							handleApply();
+						}}
+						onReset={() => handleReset()}>
+						<Text size={31} weight={800}>
+							Задайте параметры
+						</Text>
+						<Select
+							title='Шрифт'
+							options={fontFamilyOptions}
+							selected={formSettings.fontFamilyOption}
+							onChange={(value) =>
+								setFormSettings({ ...formSettings, fontFamilyOption: value })
+							}
+							placeholder='Выберите шрифт'
 						/>
-						<Button
-							title='Применить'
-							htmlType='submit'
-							type='apply'
-							onClick={handleApply}
+						<RadioGroup
+							name='Размер шрифта'
+							title='Размер шрифта'
+							options={fontSizeOptions}
+							selected={formSettings.fontSizeOption}
+							onChange={(value) =>
+								setFormSettings({ ...formSettings, fontSizeOption: value })
+							}
 						/>
-					</div>
-				</form>
-			</aside>
+						<Select
+							title='Цвет шрифта'
+							options={fontColors}
+							selected={formSettings.fontColor}
+							onChange={(value) =>
+								setFormSettings({ ...formSettings, fontColor: value })
+							}
+							placeholder='Выберите цвет шрифта'
+						/>
+						<Separator />
+						<Select
+							title='Цвет фона'
+							options={backgroundColors}
+							selected={formSettings.backgroundColor}
+							onChange={(value) =>
+								setFormSettings({ ...formSettings, backgroundColor: value })
+							}
+							placeholder='Выберите цвет фона'
+						/>
+						<Select
+							title='Ширина контента'
+							options={contentWidthArr}
+							selected={formSettings.contentWidth}
+							onChange={(value) =>
+								setFormSettings({ ...formSettings, contentWidth: value })
+							}
+							placeholder='Выберите ширину контента'
+						/>
+						<div className={styles.bottomContainer}>
+							<Button title='Сбросить' htmlType='reset' type='clear' />
+							<Button title='Применить' htmlType='submit' type='apply' />
+						</div>
+					</form>
+				</aside>
+			</div>
 		</>
 	);
-});
+};
 ArticleParamsForm.displayName = 'ArticleParamsForm';
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState, useRef, useEffect } from 'react';
-import clsx from 'clsx';
+import { StrictMode, CSSProperties, useState } from 'react';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
@@ -12,74 +11,20 @@ import {
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
-import { ArrowButton } from './ui/arrow-button';
-
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
 	// console.log('начну хоть с чего-то');
 
-	// состояние для открытия сайдбара
-	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-	// console.log('Состояние isSidebarOpen = ', isSidebarOpen);
-
 	// новое состояние для применённых настроек
 	const [appliedSettings, setAppliedSettings] =
 		useState<ArticleStateType>(defaultArticleState);
 
-	// для сайдбара
-	const sidebarRef = useRef<HTMLElement>(null);
-
-	// для стрелки
-	const arrowRef = useRef<HTMLDivElement>(null);
-
-	useEffect(() => {
-		// если сайдбар закрыт, ничего не делаем
-		if (!isSidebarOpen) return;
-
-		const clickedMouseOutside = (event: MouseEvent) => {
-			// проверяем, был ли клик внутри сайдбара
-			const isClickInsideSidebar = sidebarRef.current?.contains(
-				event.target as Node
-			);
-			// if (isClickInsideSidebar) {
-			// 	console.log('Клик произошёл на сайдбаре');
-			// }
-			// проверяем, был ли клик на стрелке
-			const isClickOnArrow = arrowRef.current?.contains(event.target as Node);
-			// if (isClickOnArrow) {
-			// 	console.log('Клик произошёл на стрелке');
-			// }
-			if (!isClickInsideSidebar && !isClickOnArrow) {
-				console.log('Клик вне стрелки и сайдбара');
-				// закрываем сайдбар
-				setIsSidebarOpen(false);
-			}
-		};
-
-		// добавляем обработчик
-		document.addEventListener('mousedown', clickedMouseOutside);
-
-		// удаляем обработчик при размонтировании или при изменении isSidebarOpen
-		return () => {
-			document.removeEventListener('mousedown', clickedMouseOutside);
-		};
-		// эффект перезапускается при изменении isSidebarOpen
-	}, [isSidebarOpen]);
-
 	return (
 		<>
-			<ArrowButton
-				ref={arrowRef}
-				isOpen={isSidebarOpen}
-				onClick={() => {
-					// console.log("клик из App, isSidebarOpen: ", isSidebarOpen);
-					setIsSidebarOpen((prev) => !prev);
-				}}
-			/>
 			<main
-				className={clsx(styles.main)}
+				className={styles.main}
 				style={
 					{
 						// используем appliedSettings вместо defaultArticleState
@@ -90,14 +35,12 @@ const App = () => {
 						'--bg-color': appliedSettings.backgroundColor.value,
 					} as CSSProperties
 				}>
-				{isSidebarOpen && (
+				{
 					<ArticleParamsForm
-						ref={sidebarRef}
-						isOpen={isSidebarOpen}
 						currentSettings={appliedSettings}
 						applySettings={setAppliedSettings}
 					/>
-				)}
+				}
 				<Article />
 			</main>
 		</>
